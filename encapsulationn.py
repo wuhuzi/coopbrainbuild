7f147ddb1283d025d02d0a0191833970dda2d139
wuhuzi <wuhuzi@163.com>
2021-01-26 21:54:59 +0800

    finish demo with encapsulation sequences

--- encapsulationn.py
+++ encapsulationn.py
@@ -52,14 +52,45 @@ import json
 }
 
 '''
+# 封装执行序列
 sequences_data_encapsulate = {}
 # response code
 sequences_data_encapsulate["response_code"] = "200"
-# 统计sequences 的长度
-a = {"skills":"init","primitives":"回","target_lable":"","target_pose":"(x,y,z)","target_angle":"θ"}
-b = {"skills":"init","primitives":"回","target_lable":"","target_pose":"(x,y,z)","target_angle":"θ"}
 
-sequences = [a,b]
+'''
+封装sequences
+    1. 动作基元获取
+    2. 动作基元参数封装
+    3. 封装sequences
+'''
+# 1. 动作基元获取 assume primitives is lists name:primitives_list
+primitives_list = ["趋","抓","提","移","放","回"]
+
+sequences = []
+for primitive in primitives_list:
+    primitive_dict = {}
+    primitive_dict["skills"] = "抓取"
+    primitive_dict["primitives"] = primitive
+    if(primitive=='趋'):
+        primitive_dict["target_lable"] = '螺母'
+        primitive_dict["target_pose"] = '(x,y,z)'
+    elif(primitive=='移'):
+        primitive_dict["target_lable"] = '螺栓'
+        primitive_dict["target_pose"] = '(x,y,z)'
+    else:
+        primitive_dict["target_lable"] = 'None'
+        primitive_dict["target_pose"] = 'None'
+
+    if (primitive == '抓'):
+        primitive_dict["target_angle"] = '100'
+    elif (primitive == '放'):
+        primitive_dict["target_angle"] = '0'
+    else:
+        primitive_dict["target_angle"] = 'None'
+    sequences.append(primitive_dict)
+
+
+# 统计sequences 的长度 并封装入json
 sequences_data_encapsulate["count"] = len(sequences)
 
 # 封装sequences
--- test_json.py
+++ test_json.py
@@ -18,27 +18,67 @@ json_str = '''
 }
 
 '''
-# 打印字符串 str
-print(type(json_str))
+# 接收实体位姿封装值
+def get_entity_param():
 
-# 打印转换好的dict ; str-->dict
-data = json.loads(json_str)
-print("json.loads(json_str) type is " +  str(type(data)))
-print("data:"+ str(data))
+    # 打印转换好的dict ; str-->dict
+    data = json.loads(json_str)
+    # param_entity is list
+    param_entity = data.get('param_entity')
+    return param_entity
 
-# param_entity is list
-print(data.get('response_code'))
-param_entity = data.get('param_entity')
-print(type(param_entity))
 
-# param_entity[0] is dict
-print("param_entity集合的长度：" + str(len(param_entity)))
-print(param_entity[0])
-print(type(param_entity[0]))
-print(param_entity[0].get("target_lable"))
-
-# 遍历集合
-for entity_list in param_entity:
-    print(entity_list.get("target_lable"))
 
 # 封装执行序列
+def encapsulate_sequences():
+    # 获取带有封装参数的实体
+    param_entity = get_entity_param()
+    print(param_entity)
+    # 开始封装执行序列
+    sequences_data_encapsulate = {}
+    # 封装 response code
+    sequences_data_encapsulate["response_code"] = "200"
+
+    '''
+    封装sequences
+        1. 动作基元获取
+        2. 动作基元参数封装
+        3. 封装sequences
+    '''
+    # 1. 动作基元获取 assume primitives is lists name:primitives_list
+    primitives_list = ["趋", "抓", "提", "移", "放", "回"]
+    sequences = []
+    for primitive in primitives_list:
+        primitive_dict = {}
+        primitive_dict["skills"] = "抓取"
+        primitive_dict["primitives"] = primitive
+        if (primitive == '趋'):
+            primitive_dict["target_lable"] = '螺母'
+            primitive_dict["target_pose"] = '(x,y,z)'
+        elif (primitive == '移'):
+            primitive_dict["target_lable"] = '螺栓'
+            primitive_dict["target_pose"] = '(x,y,z)'
+        else:
+            primitive_dict["target_lable"] = 'None'
+            primitive_dict["target_pose"] = 'None'
+
+        if (primitive == '抓'):
+            primitive_dict["target_angle"] = '100'
+        elif (primitive == '放'):
+            primitive_dict["target_angle"] = '0'
+        else:
+            primitive_dict["target_angle"] = 'None'
+        sequences.append(primitive_dict)
+
+    # 统计sequences 的长度 并封装入json
+    sequences_data_encapsulate["count"] = len(sequences)
+    # 封装sequences
+    sequences_data_encapsulate["data"] = {"sequences": sequences}
+    # 封装结果转为json
+    sequences_data_encapsulate_json = json.dumps(sequences_data_encapsulate, ensure_ascii=False)
+    print(sequences_data_encapsulate_json)
+    return sequences_data_encapsulate_json
+
+
+
+encapsulate_sequences()
